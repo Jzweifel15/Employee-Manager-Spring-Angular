c3f06b842ac693c351f274d8d88dcbc76f4878c3
Justin R. Zweifel <jzweifel15@yahoo.com>
2022-05-18 05:42:33 -0400

    Update EmployeeRepo.java
    added additional useful methods

--- employeemanager/src/main/java/com/demo/employeemanager/repos/EmployeeRepo.java
+++ employeemanager/src/main/java/com/demo/employeemanager/repos/EmployeeRepo.java
@@ -1,8 +1,12 @@
 package com.demo.employeemanager.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 import com.demo.employeemanager.models.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-    
+    void deleteEmployeeById(Long id);
+    Optional findEmployeeById(Long id);
 }
