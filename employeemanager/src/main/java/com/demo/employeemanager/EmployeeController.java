0fec76fb9b15be6a935ab3f8080b200ed7393a7e
Justin R. Zweifel <jzweifel15@yahoo.com>
2022-05-18 20:56:35 -0400

    Update EmployeeController.java
    Added CrossOrigin annotation to allow CORS

--- employeemanager/src/main/java/com/demo/employeemanager/EmployeeController.java
+++ employeemanager/src/main/java/com/demo/employeemanager/EmployeeController.java
@@ -7,6 +7,7 @@ import com.demo.employeemanager.models.Employee;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/employee")
 public class EmployeeController {
 
