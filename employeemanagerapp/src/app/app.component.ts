2e021fe6314baea74f4c6765190519cc7e3f51a0
Justin R. Zweifel <jzweifel15@yahoo.com>
2022-05-18 22:08:06 -0400

    Update app.component.ts
    Added logic for add and edit forms as well as delete functionality that will be sent to the backend, then instantly be represented within the frontend

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -12,10 +13,13 @@ export class AppComponent implements OnInit {
 
   public employees: Employee[];
 
+  public editEmployee: Employee;
+  public deleteEmployee: Employee;
+
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-      this.getEmployees();
+    this.getEmployees();
   }
 
   public getEmployees(): void {
@@ -28,4 +32,64 @@ export class AppComponent implements OnInit {
       }
     )
   }
+
+  public onAddEmloyee(addForm: NgForm): void {
+    document.getElementById("add-employee-form")?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onEditEmployee(employee: Employee): void {
+    this.employeeService.editEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteEmployee(id: number): void {
+    this.employeeService.deleteEmployee(id).subscribe(
+      (response: any) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(employee: any, mode: string): void {
+    const container = document.getElementById("main-container");
+    const button = document.createElement("button");
+    button.type = "button";
+    button.style.display = "none";
+    button.setAttribute("data-toggle", "modal");
+
+    if (mode === "add") {
+      button.setAttribute("data-target", "#addEmployeeModal");
+    }
+    else if (mode === "edit") {
+      this.editEmployee = employee;
+      button.setAttribute("data-target", "#editEmployeeModal");
+    }
+    else if (mode === "delete") {
+      this.deleteEmployee = employee;
+      button.setAttribute("data-target", "#deleteEmployeeModal");
+    }
+
+    container?.appendChild(button);
+    button.click();
+  } 
 }
